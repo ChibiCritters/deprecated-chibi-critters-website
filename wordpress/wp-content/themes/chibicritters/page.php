5c579027ebc83e6090eda22ae0d2fd0055aa5e2b
Daniela Howe <danidolphin@gmail.com>
2015-02-01 22:49:41 -0700

    Fixed issue #9. Site is no longer to narrow

--- wordpress/wp-content/themes/chibicritters/page.php
+++ wordpress/wp-content/themes/chibicritters/page.php
@@ -15,34 +15,36 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
+			<div class="container">
+				<div class="col-md-10 col-md-offset-1">
+					<?php /* The loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header class="entry-header">
+								<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+								<div class="entry-thumbnail">
+									<?php the_post_thumbnail(); ?>
+								</div>
+								<?php endif; ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-						<div class="entry-thumbnail">
-							<?php the_post_thumbnail(); ?>
-						</div>
-						<?php endif; ?>
+								<h1 class="entry-title"><?php the_title(); ?></h1>
+							</header><!-- .entry-header -->
 
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header><!-- .entry-header -->
+							<div class="entry-content">
+								<?php the_content(); ?>
+								<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'chibicritters' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+							</div><!-- .entry-content -->
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'chibicritters' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-					</div><!-- .entry-content -->
+							<footer class="entry-meta">
+								<?php edit_post_link( __( 'Edit', 'chibicritters' ), '<span class="edit-link">', '</span>' ); ?>
+							</footer><!-- .entry-meta -->
+						</article><!-- #post -->
 
-					<footer class="entry-meta">
-						<?php edit_post_link( __( 'Edit', 'chibicritters' ), '<span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
-				</article><!-- #post -->
-
-				<?php comments_template(); ?>
-			<?php endwhile; ?>
-
+						<?php comments_template(); ?>
+					<?php endwhile; ?>
+				</div>
+			</div>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
